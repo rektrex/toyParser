845bc40f70516727fc344e26bab30b959c5073dd
rektrex <23721797+rektrex@users.noreply.github.com>
2020-06-29 17:21:57 +0530

    add few more combinators

--- src/Combinators.hs
+++ src/Combinators.hs
@@ -1,6 +1,7 @@
 module Combinators where
 
 import Class
+import Control.Applicative
 
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy f = P $ \st ->
@@ -24,3 +25,25 @@ string (c:cs) = (:) <$> char c <*> string cs
 
 oneOf :: [Char] -> Parser Char
 oneOf cs = satisfy (`elem` cs)
+
+choice :: Alternative m => [m a] -> m a
+choice = foldr (<|>) empty
+
+option :: Alternative m => a -> m a -> m a
+option x p = p <|> pure x
+
+skipOptional :: Alternative m => m a -> m ()
+skipOptional p = (() <$ p) <|> pure ()
+
+between :: Applicative m => m start -> m end -> m a -> m a
+between s e p = s *> p <* e
+
+sepBy1 :: Alternative m => m a -> m sep -> m [a]
+sepBy1 p sep = (:) <$> p <*> many (sep *> p)
+
+sepBy :: Alternative m => m a -> m sep -> m [a]
+sepBy p sep = sepBy1 p sep <|> pure []
+
+count :: Applicative m => Int -> m a -> m [a]
+count n p | n <= 0 = pure []
+          | otherwise = sequenceA (replicate n p)
